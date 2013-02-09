cb0d21b8213f4ca00cf16c336e3614eff6f4418b
gtscherer <gtscherer@gmail.com>
2013-02-09 12:56:21 -0700

    started Pricing Model

--- CSE445_Project2_Console/PricingModel.cs
+++ CSE445_Project2_Console/PricingModel.cs
@@ -12,5 +12,11 @@ namespace CSE445_Project2_Console
          * You must define a mathematical model (formula) to determine the price based on the order received within a given time period and the number of rooms the HotelSupplier available in the same time period.
          * You can use a hard-coded table of the price in each week day, however you must make sure your model will allow the price to go up for some period of time and go down some other time.
          */
-    }
+        public void PricingModel(){
+                int[] priceTable = new int[7];
+                for(int i = 0; i < priceTable.Length; ++i)
+                {
+
+                }
+        }
 }

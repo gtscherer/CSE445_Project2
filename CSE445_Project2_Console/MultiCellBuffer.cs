ab97b71553098fb9694890f440b0168e15dd808f
Alan Gieske <amgieske@asu.edu>
2013-02-12 00:05:23 -0700

    MultiCellBuffer created

--- CSE445_Project2_Console/MultiCellBuffer.cs
+++ CSE445_Project2_Console/MultiCellBuffer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace CSE445_Project2_Console
 {
@@ -21,5 +22,86 @@ namespace CSE445_Project2_Console
          * 
          * 
          */
+
+        /*
+         * Starting point for the multicell buffer.
+         * Might need changes when we integrate to meet the synchronization requirement
+         * Cycle method for the getOneCell should prevent any threads from starving
+         * Needs testing once we have a travel agency class put together
+         */
+
+        static int numberOfCells = 5;
+
+        //Semaphore allows travel agency to get access, hotel supplier will release when order is processed
+        public static Semaphore _cells = new Semaphore(numberOfCells , numberOfCells);
+
+
+        static string[] cells = new string[numberOfCells];
+        static int counter = 0;
+
+        public void setOneCell(String order)
+        {           
+                bool success = false;
+                
+                //Cycle through the cells
+                for (int i = 0; i < cells.Length && !success; ++i)
+                {
+                    //Find the first empty cell
+                    if (cells[i] == null)
+                    {
+                        //Fill it with the order
+                        cells[i] = order;
+                        success = true;
+                    }
+                }
+            
+                //Should only happen if we fail to use the semaphore
+                if (!success)
+                {
+                    Console.WriteLine("Cells are full, Could not write to buffer");
+                }            
+        }
+
+        //Test to make certain the cells aren't empty
+        public bool cellsEmpty()
+        {
+            bool empty = true;
+            for (int i = 0; i < cells.Length; ++i)
+            {
+                if (cells[i] != null)
+                    empty = false;
+            }
+            return empty;
+        }
+
+        //Grabs the contents of one cell, ensuring to cycle through all the cells
+        public string getOneCell()
+        {
+            string order = null;
+            //Ensure there's an order to get
+            if (!cellsEmpty())
+            {
+
+                //cycle until we find an occupied cell
+                while (cells[counter] == null)
+                {
+                    counter = (counter + 1) % cells.Length;
+                }
+
+                //get the cell contents
+                order = cells[counter];
+
+                //Delete the cell contents
+                cells[counter] = null;
+
+                //Move the counter to the next slot
+                counter = (counter + 1) % cells.Length;
+
+            }
+
+            //Return the found order
+            return order;
+        }
+
     }
 }
